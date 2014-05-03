5170ad148d243d5150a291b2be45e43654f187f6
primenumber <primenumber.proj@gmail.com>
2014-05-03 16:02:49 +0900

    評価関数を複数の関数に分割

--- score.cpp
+++ score.cpp
@@ -5,35 +5,50 @@
 constexpr int sorted_weight  = 100;
 constexpr int corner_weight  = 100;
 constexpr int zero_weight    =  10;
-constexpr int same_weight    =  15;
+constexpr int same_weight    =  20;
 constexpr int movable_weight =  50;
 
-int static_score(const table_t& table) {
+int zero_score(const table_t& table) {
   int zero_number = 0;
-  int same_tile_number = 0;
-  int max_number = 0;
+  for (int i = 0; i < 4; ++i)
+    for (int j = 0; j < 4; ++j)
+      if (table[i][j] == 0)
+        ++zero_number;
+  return zero_number;
+}
+
+int movable_score(const table_t& table) {
   int movable_number = 0;
-  for (int i = 0; i < 4; ++i) {
+  for (int i = 0; i < 4; ++i)
     if (movable(table, i)) ++movable_number;
-  }
-  std::array<bool, 16> sorted;
-  std::array<int, 16> sorted_sum;
-  std::fill(std::begin(sorted), std::end(sorted), true);
-  std::fill(std::begin(sorted_sum), std::end(sorted_sum), 0);
+  return (movable_number <= 1) ? -1 : 0;
+}
+
+int same_score(const table_t& table) {
+  int same_number = 0;
   int di[] = {-1, 0, 1, 0};
   int dj[] = {0, 1, 0, -1};
   for (int i = 0; i < 4; ++i) {
     for (int j = 0; j < 4; ++j) {
-      max_number = std::max(max_number, table[i][j]);
-      if (table[i][j] == 0)
-        ++zero_number;
       for (int k = 0; k < 4; ++k) {
         int ni = i + di[k];
         int nj = j + dj[k];
         if (ni < 0 || ni >= 4 || nj < 0 || nj >= 4) continue;
         if (table[i][j] == table[ni][nj])
-          ++same_tile_number;
+          ++same_number;
       }
+    }
+  }
+  return same_number / 2;
+}
+
+int sorted_score(const table_t& table) {
+  std::array<bool, 16> sorted;
+  std::array<int, 16> sorted_sum;
+  std::fill(std::begin(sorted), std::end(sorted), true);
+  std::fill(std::begin(sorted_sum), std::end(sorted_sum), 0);
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
       sorted_sum[i] += table[i][j];
       sorted_sum[i+4] += table[i][j];
       sorted_sum[i+8] += table[j][i];
@@ -50,17 +65,25 @@ int static_score(const table_t& table) {
         sorted[i+12] = false;
     }
   }
-  int sorted_score = 0;
+  int score = 0;
   for (int i = 0; i < 16; ++i)
     if (sorted[i])
-      sorted_score += sorted_sum[i];
-  int corner_score = table[0][0] + table[0][3] + table[3][0] + table[3][3];
-  int movable_score = 0;
-  if (movable_number <= 1)
-    movable_score = max_number;
-  return sorted_score * sorted_weight
-      + corner_score * corner_weight
-      + same_tile_number * max_number * same_weight
-      + zero_number * max_number * zero_weight
-      + movable_score * movable_weight;
+      score += sorted_sum[i];
+  return score;
+}
+
+int corner_score(const table_t& table) {
+  return table[0][0] + table[0][3] + table[3][0] + table[3][3];
+}
+
+int static_score(const table_t& table) {
+  int max_number = 0;
+  for (int i = 0; i < 4; ++i)
+    for (int j = 0; j < 4; ++j)
+      max_number = std::max(max_number, table[i][j]);
+  return sorted_score(table) * sorted_weight
+      + corner_score(table) * corner_weight
+      + same_score(table) * max_number * same_weight
+      + zero_score(table) * max_number * zero_weight
+      + movable_score(table) * max_number * movable_weight;
 }
