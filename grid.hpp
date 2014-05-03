f387c7a9fdd4450378aa2c284beeff3495366230
primenumber <primenumber.proj@gmail.com>
2014-05-03 09:03:21 +0900

    fix

--- grid.hpp
+++ grid.hpp
@@ -5,6 +5,6 @@
 using table_t = std::array<std::array<int, 4>, 4>;
 
 table_t rotate(table_t table, int direction);
-table_t movable(const table_t& table, int direction);
+bool movable(const table_t& table, int direction);
 table_t move(const table_t& table, int direction);
 std::vector<std::pair<int, int>> zero_list(const table_t& table);
