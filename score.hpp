89760fb3664f12662ab22c3d55781b9b23619859
primenumber <primenumber.proj@gmail.com>
2014-05-05 03:27:34 +0900

    use Monte-Carlo method

--- score.hpp
+++ score.hpp
@@ -1,3 +1,4 @@
 #include "grid.hpp"
 
 int static_score(const table_t& table);
+int static_score_light(const table_t& table);
--- search.hpp
+++ search.hpp
@@ -2,6 +2,8 @@
 #include "score.hpp"
 
 int search(const table_t& table);
-int alpha_beta_host(const table_t& table, int alpha, int beta, int depth);
-int alpha_beta_player(table_t&& table, int alpha, int beta, int depth);
+
+int alpha_beta_player(const table_t& table, int alpha, int beta, int depth);
+int alpha_beta_host(table_t&& table, int alpha, int beta, int depth);
 int alpha_beta_search(const table_t& table);
+int Monte_Carlo_search(const table_t& table);
