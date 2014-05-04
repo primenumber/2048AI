966c6caadd5204b6b2086c32bfbea5153e2e002f
primenumber <prime@kmc.gr.jp>
2014-05-04 23:55:22 +0900

    modify search.cpp

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-2048ai:2048ai.cpp grid.cpp grid.hpp score.cpp score.hpp
-	g++ 2048ai.cpp grid.cpp score.cpp --std=c++11 -o 2048ai
+2048ai:2048ai.cpp grid.cpp grid.hpp score.cpp score.hpp search.hpp search.cpp
+	g++ 2048ai.cpp grid.cpp score.cpp search.cpp --std=c++11 -o 2048ai
--- search.hpp
+++ search.hpp
@@ -1,3 +1,7 @@
 #include "grid.hpp"
+#include "score.hpp"
 
 int search(const table_t& table);
+int alpha_beta_host(const table_t& table, int alpha, int beta, int depth);
+int alpha_beta_player(table_t&& table, int alpha, int beta, int depth);
+int alpha_beta_search(const table_t& table);
