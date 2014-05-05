055f41c977aac9d36bc9f04f4bb2d410cd9f5e67
primenumber <prime@kmc.gr.jp>
2014-05-05 20:11:40 +0900

    add Window_Wrapper

--- wrapper.rb
+++ wrapper.rb
@@ -15,26 +15,40 @@ def get_json(location)
   return JSON.parse(text)
 end
 
-def set_num(i, j, t_id, num)
-  if num > 0 then
-    str = sprintf("%6d", num)
-  else
-    str = "      "
-  end
-  id = 0
-  while num > 1
-    id += 1
-    num /= 2
-  end
-  $grid[t_id][i][j].attrset(Curses::color_pair(id) | Curses::A_BOLD) if id > 0
-  $grid[t_id][i][j].setpos(1,1)
-  $grid[t_id][i][j].addstr(str)
-  $grid[t_id][i][j].attroff(Curses::A_COLOR | Curses::A_BOLD)
-  $grid[t_id][i][j].refresh
-end
-
 class Game
-  def start(thread_id)
+  def init_grid
+    @grid = Array.new(4)
+    for i in 0...4
+      @grid[i] = Array.new(4)
+    end
+    cnt = 0
+    @window.children.each do |child|
+      i = cnt / 4
+      j = cnt % 4
+      @grid[i][j] = child
+      cnt += 1
+    end
+  end
+  def set_num(i, j, num)
+    if num > 0 then
+      str = sprintf("%6d", num)
+    else
+      str = "      "
+    end
+    id = 0
+    while num > 1
+      id += 1
+      num /= 2
+    end
+    @grid[i][j].attrset(Curses::color_pair(id) | Curses::A_BOLD) if id > 0
+    @grid[i][j].setpos(1,1)
+    @grid[i][j].addstr(str)
+    @grid[i][j].attroff(Curses::A_COLOR | Curses::A_BOLD)
+    @grid[i][j].refresh
+  end
+  def start(window)
+    @window = window
+    init_grid
     json_data = get_json(HOST+'/hi/start/json')
     @session_id = json_data["session_id"]
     loop do
@@ -46,20 +60,69 @@ class Game
         end
       end
       move = (`echo #{str} | ./2048ai`).to_i
-      Curses::setpos(19,thread_id*40)
-      Curses::addstr("move: " + $move_direction_str[move])
+      @window.setpos(19, 1)
+      @window.addstr("move: " + $move_direction_str[move])
       json_data = get_json("#{HOST}/hi/state/#{@session_id}/move/#{move}/json")
       for i in 0...4
         for j in 0...4
-          set_num(i, j, thread_id, json_data["grid"][i][j])
+          set_num(i, j, json_data["grid"][i][j])
         end
       end
-      Curses::setpos(20,thread_id * 40)
-      Curses::addstr("Score: #{sprintf("%7d",json_data["score"])}")
-      Curses::refresh
+      @window.setpos(20, 1)
+      @window.addstr("Score: #{sprintf("%7d",json_data["score"])}")
+      @window.refresh
       break if json_data["over"]
     end
   end
+end
+
+class Window_Wrapper
+  def initialize(window = Curses::stdscr, offsety = 0, offsetx = 0)
+    @window = window
+    @offsety = offsety
+    @offsetx = offsetx
+    @children = Array.new
+  end
+  def subwin(height, width, y, x)
+    child_raw = @window.subwin(height, width, y + @offsety, x + @offsetx)
+    child = Window_Wrapper.new(child_raw, y + @offsety, x + @offsetx)
+    children.push(child)
+    return child
+  end
+  def setpos(y, x)
+    @window.setpos(y, x)
+  end
+  def addstr(str)
+    @window.addstr(str)
+  end
+  def refresh
+    @window.refresh
+  end
+  def box(ch1,ch2,ch3)
+    @window.box(ch1,ch2,ch3)
+  end
+  def attrset(arg)
+    @window.attrset(arg)
+  end
+  def attroff(arg)
+    @window.attroff(arg)
+  end
+  def children
+    return @children
+  end
+end
+
+def init_window(window)
+  grid = Array.new(4)
+  for i in 0...4
+    grid[i] = Array.new(4)
+    for j in 0...4
+      grid[i][j] = window.subwin(3, 8, i * 3 + 2, j * 9 + 1)
+      grid[i][j].box(?|,?-,?+)
+      grid[i][j].refresh
+    end
+  end
+  return grid
 end
 
 Curses::init_screen
@@ -83,20 +146,17 @@ Curses::init_pair(15 ,Curses::COLOR_WHITE  ,Curses::COLOR_BLACK  )
 Curses::init_pair(16 ,Curses::COLOR_WHITE  ,Curses::COLOR_BLACK  )
 Curses::init_pair(17 ,Curses::COLOR_BLACK  ,Curses::COLOR_CYAN   )
 
-Curses::setpos(0, 0)
-Curses::addstr("great AI - 2048 AI")
-Curses::refresh
-$grid = Array.new(2)
+$root_window = Window_Wrapper.new
+$root_window.setpos(0, 0)
+$root_window.addstr("great AI - 2048 AI")
+$root_window.refresh
+$window = Array.new(2)
+$grids = Array.new(2)
 for i in 0...2
-  $grid[i] = Array.new(4)
-  for j in 0...4
-    $grid[i][j] = Array.new(4)
-    for k in 0...4
-      $grid[i][j][k] = Curses::stdscr.subwin(3, 8, j * 3 + 2, i * 40 + k * 9 + 1)
-      $grid[i][j][k].box(?|,?-,?+)
-      $grid[i][j][k].refresh
-    end
-  end
+  $window[i] = $root_window.subwin(22, 49, 1, 50 * i)
+  $window[i].box(?|,?-,?+)
+  $grids[i] = init_window($window[i])
+  $window[i].refresh
 end
 loop do
   break if Curses::getch == ?q
@@ -104,7 +164,7 @@ loop do
   for i in 0..1
     threads.push(Thread.start(i) {|i|
       game = Game.new
-      game.start(i)
+      game.start($window[i])
     })
   end
   threads.each{|th| th.join}
