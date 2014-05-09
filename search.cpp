fe8fb6578a4dc18c41521c626c8674e3bd286c28
primenumber <primenumber.proj@gmail.com>
2014-05-10 02:29:12 +0900

    modify search algorithm and monte carlo algorithm

--- search.cpp
+++ search.cpp
@@ -11,35 +11,64 @@ extern std::mt19937 mt;
 namespace ai2048 {
 namespace search {
 
-int alpha_beta_host(grid::Grid grid, std::map<grid::Grid, int>& scores, int alpha, int beta, int depth) {
+int alpha_beta_player(const grid::Grid& grid,
+    std::map<grid::Grid, Value>& scores,
+    int alpha, int beta, int depth) {
+  for (int j = 0; j < 4; ++j) {
+    if (!grid.is_movable(j)) continue;
+    grid::Grid next_grid = grid.move(j);
+    int score;
+    auto score_itr = scores.find(next_grid);
+    if (score_itr == end(scores)) {
+      score = alpha_beta_host(next_grid, scores, alpha, beta, depth - 1);
+      scores.insert(std::make_pair(next_grid, Value(score, alpha, beta)));
+    } else {
+      if (score_itr->second.beta < alpha
+          && score_itr->second.value == score_itr->second.beta) {
+        score = alpha_beta_host(next_grid, scores, alpha, beta, depth - 1);
+        score_itr->second.value = score;
+        if (score > alpha) {
+          score_itr->second.beta = beta;
+        } else {
+          score_itr->second.alpha = score_itr->second.beta;
+          score_itr->second.beta = alpha;
+        }
+      } else if (score_itr->second.alpha > beta
+          && score_itr->second.value == score_itr->second.alpha) {
+        score = alpha_beta_host(next_grid, scores, alpha, beta, depth - 1);
+        score_itr->second.value = score;
+        if (score < beta) {
+          score_itr->second.alpha = alpha;
+        } else {
+          score_itr->second.beta = score_itr->second.alpha;
+          score_itr->second.alpha = beta;
+        }
+      } else {
+        score = std::max(alpha, std::min(beta, score_itr->second.value));
+      }
+    }
+    if (score > alpha) {
+      alpha = score;
+      if (alpha >= beta) {
+        return beta;
+      }
+    }
+  }
+  return alpha;
+}
+
+int alpha_beta_host(grid::Grid grid,
+    std::map<grid::Grid, Value>& scores,
+    int alpha, int beta, int depth) {
   if (depth == 0) return score::static_score_light(grid);
   auto zeros = grid.zero_tiles();
   int numbers[2] = {2, 4};
   for (auto tile : zeros) {
     for (int i = 0; i < 2; ++i) {
       grid.table[tile.first][tile.second] = numbers[i];
-      int local_alpha = alpha;
-      int local_beta = beta;
-      for (int j = 0; j < 4; ++j) {
-        if (!grid.is_movable(j)) continue;
-        grid::Grid next_grid = grid.move(j);
-        int score;
-        if (scores.count(next_grid) == 0) {
-          score = alpha_beta_host(next_grid, scores, alpha, beta, depth - 1);
-          scores.insert(std::make_pair(next_grid, score));
-        } else {
-          score = scores[next_grid];
-        }
-        if (score > local_alpha) {
-          local_alpha = score;
-          if (local_alpha >= local_beta) {
-            local_alpha = local_beta;
-            break;
-          }
-        }
-      }
-      if (local_alpha < beta) {
-        beta = local_alpha;
+      int score = alpha_beta_player(grid, scores, alpha, beta, depth);
+      if (score < beta) {
+        beta = score;
         if (alpha >= beta) {
           return alpha;
         }
@@ -52,16 +81,18 @@ int alpha_beta_host(grid::Grid grid, std::map<grid::Grid, int>& scores, int alph
 
 int alpha_beta_search(const grid::Grid& grid) {
   int optimal = -1;
-  std::map<grid::Grid, int> scores;
-  for (int kDepth = 4; kDepth < 10; ++kDepth) {
+  int now_score = score::static_score(grid);
+  for (int kDepth = 4; kDepth < 12; ++kDepth) {
+    std::map<grid::Grid, Value> scores;
     auto start = std::chrono::system_clock::now();
     int alpha = -1000000000;
     int beta = 1000000000;
     for (int i = 0; i < 4; ++i) {
       if (!grid.is_movable(i)) continue;
       grid::Grid moved = grid.move(i);
+      alpha_beta_host(std::move(moved), scores, now_score - 1, now_score, kDepth - 1);
       int score = alpha_beta_host(std::move(moved), scores, alpha, beta, kDepth - 1);
-      scores.insert(std::make_pair(moved, score));
+      scores.insert(std::make_pair(moved, Value(score, alpha, beta)));
       if (score > alpha || optimal == -1) {
         alpha = score;
         optimal = i;
@@ -69,7 +100,10 @@ int alpha_beta_search(const grid::Grid& grid) {
     }
     auto end = std::chrono::system_clock::now();
     auto elapsed = end - start;
-    if (elapsed.count() > 2000000) break;
+    if (elapsed.count() > 50000) {
+      std::cout << "{\"type\":\"debug\",\"score\":" << kDepth << "}" << std::endl;
+      break;
+    }
   }
   assert(optimal != -1);
   return optimal;
@@ -92,19 +126,19 @@ std::pair<int64_t, int> playout(const grid::Grid& grid, int direction) {
   auto zeros = moved.zero_tiles();
   int z = mt() % zeros.size();
   moved.table[zeros[z].first][zeros[z].second] = ((mt() % 2) >= 1) ? 2 : 4;
+  auto movable = moved.movable_directions();
   int max_score = 0.0;
-  int max_i = -1;
-  for (int i = 0; i < 4; ++i) {
-    if (moved.is_movable(i)) {
-      int score = score::static_score_light(moved.move(i));
-      if (score > max_score) {
-        max_score = score;
-        max_i = i;
-      }
-    }
-  }
-  if (max_i != -1) {
-    auto result = playout(moved, max_i);
+  //int max_i = -1;
+  //for (int dir : movable) {
+  //  int score = score::static_score_light(moved.move(dir));
+  //  if (score > max_score) {
+  //    max_score = score;
+  //    max_i = dir;
+  //  }
+  //}
+  if (!movable.empty()) {
+    int dir = movable[mt() % movable.size()];
+    auto result = playout(moved, dir);
     return std::make_pair(result.first + 1, result.second + 1);
   } else {
     return std::make_pair(moved.sum_tiles(), 1);
@@ -124,7 +158,7 @@ int Monte_Carlo_search(const grid::Grid& grid) {
     playout_count += result.second;
   }
   int sum = grid.sum_tiles();
-  for (int i = 0; i < 80 || playout_count < 5000; ++i) {
+  for (int i = 0; i < 300 || playout_count < 25000; ++i) {
     double max_ucb1 = 0.0;
     int max_ucb1_play = -1;
     for (int j = 0; j < movable_list.size(); ++j) {
--- search.hpp
+++ search.hpp
@@ -6,9 +6,16 @@
 namespace ai2048 {
 namespace search {
 
+struct Value {
+  Value(int v, int a, int b) : value(v), alpha(a), beta(b) {};
+  int value;
+  int alpha;
+  int beta;
+};
+
 int search(const grid::Grid& grid);
 
-int alpha_beta_host(grid::Grid grid, std::map<grid::Grid, int>& scores, int alpha, int beta, int depth);
+int alpha_beta_host(grid::Grid grid, std::map<grid::Grid, Value>& scores, int alpha, int beta, int depth);
 int alpha_beta_search(const grid::Grid& grid);
 int Monte_Carlo_search(const grid::Grid& grid);
 
